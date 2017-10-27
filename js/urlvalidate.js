bb77db4d8afaacc44d151f76a3a7da82c5785b67
liufeng <lftzzzz@hotmail.com>
2017-10-27 17:44:49 +0800

    finish publish all

--- js/urlvalidate.js
+++ js/urlvalidate.js
@@ -7,11 +7,11 @@ function urlValidate(url) {
         requestUrl = 'jiemian.php';
     } else if (url.match(/^https:\/\/www.huxiu.com\/article\/[A-Za-z0-9_.\/]+.html$/)) {
         requestUrl = 'huxiu.php';
-    } else if (url.match(/^http:\/\/www.yidianzixun.com\/article\/[A-Za-z0-9_.\/]+$/)) {
+    } else if (url.match(/^https:\/\/www.yidianzixun.com\/article\/[A-Za-z0-9_.\/]+$/)) {
         requestUrl = 'yidianzixun.php';
     } else if (url.match(/^https:\/\/www.pencilnews.cn\/p\/[0-9]+.html$/)) {
         requestUrl = 'pencilnews.php';
-    } else if (url.match(/^http:\/\/www.toutiao.com\/a[0-9]+(\/)*$/)) {
+    } else if (url.match(/^https:\/\/www.toutiao.com\/a[0-9]+(\/)*$/)) {
         requestUrl = 'toutiao.php';
     }
 
