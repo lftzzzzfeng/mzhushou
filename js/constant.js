2af44bdb2c23e4fdf5599912fa317ae99a68f025
hhd <283362821@qq.com>
2017-11-08 11:51:26 +0800

    切换

--- js/constant.js
+++ js/constant.js
@@ -3,14 +3,14 @@
 //var REQUEST_DOMAIN = 'http://192.168.0.113/zhushou/api/';
 //var REQUEST_DOMAIN = 'http://192.168.0.112/zhushou/api/';
 //var REQUEST_DOMAIN = 'http://192.168.0.104/zhushou/api/';
-var REQUEST_DOMAIN = 'http://192.168.0.113/zhanyezhushou/api/';
-//var REQUEST_DOMAIN = 'http://zs.meijiexia.cn/api/';
+//var REQUEST_DOMAIN = 'http://192.168.0.113/zhanyezhushou/api/';
+var REQUEST_DOMAIN = 'http://zs.meijiexia.cn/api/';
 //var PREVIOUS_REQUEST_DOMAIN = 'http://192.168.199.192/zhushou/';
 //var PREVIOUS_REQUEST_DOMAIN = 'http://192.168.0.113/zhushou/';
 //var PREVIOUS_REQUEST_DOMAIN = 'http://192.168.0.112/zhushou/';
 //var PREVIOUS_REQUEST_DOMAIN = 'http://192.168.0.104/zhushou/';
-var PREVIOUS_REQUEST_DOMAIN = 'http://192.168.0.113/zhanyezhushou/';
-//var PREVIOUS_REQUEST_DOMAIN = 'http://zs.meijiexia.cn/';
+//var PREVIOUS_REQUEST_DOMAIN = 'http://192.168.0.113/zhanyezhushou/';
+var PREVIOUS_REQUEST_DOMAIN = 'http://zs.meijiexia.cn/';
 
 var AD_TEMPLATE_ID = 0;
 var CRAWLING_URL = 'crawlingUrl';
@@ -22,7 +22,7 @@ var RETURN_TYPE_JSON = 2;
 var TOKEN = 'token';
 var USER_INFO = 'userInfo';
 
-var IMG_URL = 'http://192.168.0.113/zhanyezhushou/img0.php';
-//var IMG_URL = 'http://zs.meijiexia.cn/img0.php';
+//var IMG_URL = 'http://192.168.0.113/zhanyezhushou/img0.php';
+var IMG_URL = 'http://zs.meijiexia.cn/img0.php';
 var QIAN_QUAN_SHOW = 1;
 var WEIXIN_BJ = '/editor/attached/image/20170221/20170221175856_40586.jpg';
